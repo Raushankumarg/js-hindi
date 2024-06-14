4569fcb520f7c4773d27b68de54edbc2d978fc90
Raushan kumar <raushankumar00125400@gmail.com>
2024-06-14 07:49:52 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=12345
+let accouuntEmail="rk1234@gmail.com"
+var accountPassword="9771"
+let accountCity="muzffarpur"
+
+
+// accountId= 899899
+
+console.log(accouuntPassword); 
+
+
+/* prefer nit to use  var 
+ becouse issue in blank scope and functional scope */
+
+
+
+console.table([accountId, accouuntEmail, accountPassword, accountCity])
+
+
+
