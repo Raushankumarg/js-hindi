fd75fb407cfe5e5f5ddb8c6bd85fcb470072f21c
Raushan kumar <raushankumar00125400@gmail.com>
2024-06-14 07:49:11 +0000

    datatype

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all js code as newer varsion
+
+console.log( 3
+    +
+    3) // code readability should be high
+
+
+let name ="raushan"
+let age = 18
+let isLoggedin = true
+
+
+
+
+// number=> 2 to power 53
+// string => ""
+// Boolean => true/false
+// null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
+
+
+
+
+
+
+
+
+
+
+
